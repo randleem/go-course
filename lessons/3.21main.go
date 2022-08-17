c0b57903e08ab7e887a0424386a26022458cd570
randleem <erandle2211@gmail.com>
2022-08-17 14:56:07 +0100

    http requrst and starting an application

---
+++ lessons/3.21main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		n, err := fmt.Fprintf(w, "Hello World")
+		if err != nil{
+			fmt.Println("error occured", err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
+
+}
