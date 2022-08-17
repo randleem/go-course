f36d2224d5081625fc20c20dceb82bc0e8c80dcc
randleem <erandle2211@gmail.com>
2022-08-17 11:36:41 +0100

    go channels

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,7 +1,17 @@
 package helpers
 
+import (
+	"math/rand"
+	"time"
+)
+
 type SomeStruct struct {
 	TypeName string
 	TypeNumber int
 }
 
+func RandomNumber(n int) int{
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(n)
+	return value
+}
